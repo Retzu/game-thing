abb30b8d52b79698103247a6101956ab021d8ab7
Christoph Willinger <christoph.willinger@gmail.com>
2014-10-09 18:39:31 +0200

    Added awesome gulp server for local development

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var mainBowerFiles = require('main-bower-files');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
+var connect = require('gulp-connect');
 
 gulp.task('js', function() {
     gulp.src('devel/js/*.js')
         .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('public/js/'));
+        .pipe(gulp.dest('public/js/'))
+        .pipe(connect.reload());
 });
 
 gulp.task('bower-files', function(){
@@ -19,6 +21,15 @@ gulp.task('bower-files', function(){
 
 gulp.task('watch', function() {
     gulp.watch('devel/js/*.js', ['js']);
+});
+
+gulp.task('server', function() {
+    gulp.start('js');
+    connect.server({
+       root: 'public',
+       livereload: true
+    });
+    gulp.start('watch');
 });
 
 gulp.task('default', ['bower-files', 'js']);
